dd9e70805731c2a7b32cab03aed8d4335c10aff3
John <J.G.Lokerse@gmail.com>
2020-08-23 20:14:26 +0200

    Removed try-catch

--- azure-deploy/deploy.sh
+++ azure-deploy/deploy.sh
@@ -2,21 +2,17 @@ rgName="rg-githubactions-demo"
 sku="B1"
 location="westeurope"
 
-{
-    # Create resource group
-    az group create --location $location --resource-group $rgName
 
-    # Create AppService
-    appServiceName="appservice-euw-githubactionsdemo"
-    az appservice plan create --location $location --name $appServiceName --resource-group $rgName --sku $sku
+# Create resource group
+az group create --location $location --resource-group $rgName
 
-    # Create WebApp
-    webAppName="webapp-euw-githubactionsdemo"
-    az webapp create --name $webAppName --resource-group $rgName --plan $appServiceName
+# Create AppService
+appServiceName="appservice-euw-githubactionsdemo"
+az appservice plan create --location $location --name $appServiceName --resource-group $rgName --sku $sku
 
-    # Deploy ZIP
-    az webapp deployment source config-zip --src .\projectoutput.zip --resource-group $rgName --name $webAppName    
-}
-|| {
-    az group delete --resource-group $rgName --yes
-}
+# Create WebApp
+webAppName="webapp-euw-githubactionsdemo"
+az webapp create --name $webAppName --resource-group $rgName --plan $appServiceName
+
+# Deploy ZIP
+az webapp deployment source config-zip --src .\projectoutput.zip --resource-group $rgName --name $webAppName    
