e268d451fe7ce8b6a0d698f511e54bf148fc6cf2
John <J.G.Lokerse@gmail.com>
2020-08-23 16:33:27 +0200

    Updated yml & deploy script

--- azure-deploy/deploy.ps1
+++ azure-deploy/deploy.ps1
@@ -1,10 +1,20 @@
 [string] $rgName = "rg-githubactions-demo"
-[string] $sku = "D1"
+[string] $sku = "B1"
 [string] $location = "westeurope"
 
 try {
     # Create resource group
     az group create --location $location --resource-group $rgName
+
+    # Create AppService
+    [string] $appServiceName = "appservice-euw-githubactionsdemo"
+    az appservice plan create --location $location --name $appServiceName --resource-group $rgName --sku $sku
+
+    # Create WebApp
+    [string] $webAppName = "webapp-euw-githubactionsdemo"
+    az webapp create --name $webAppName --resource-group $rgName --plan $appServiceName
+
+    az webapp deployment source config-zip --src .\projectoutput.zip --resource-group $rgName --name $webAppName    
 }
 catch {
     az group delete --resource-group $rgName --yes
