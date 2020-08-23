46ecfea23d0cb5ec9bf9b487fb3ad34a56470a6d
John <J.G.Lokerse@gmail.com>
2020-08-23 18:14:30 +0200

    Updated deploy script

--- azure-deploy/deploy.sh
+++ azure-deploy/deploy.sh
@@ -1,17 +1,17 @@
-rgName = "rg-githubactions-demo"
-sku = "B1"
-location = "westeurope"
+rgName="rg-githubactions-demo"
+sku="B1"
+location="westeurope"
 
 {
     # Create resource group
     az group create --location $location --resource-group $rgName
 
     # Create AppService
-    appServiceName = "appservice-euw-githubactionsdemo"
+    appServiceName="appservice-euw-githubactionsdemo"
     az appservice plan create --location $location --name $appServiceName --resource-group $rgName --sku $sku
 
     # Create WebApp
-    webAppName = "webapp-euw-githubactionsdemo"
+    webAppName="webapp-euw-githubactionsdemo"
     az webapp create --name $webAppName --resource-group $rgName --plan $appServiceName
 
     # Deploy ZIP
